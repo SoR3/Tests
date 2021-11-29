f92cb1098498589b8bbd1252007ff9bb174b4897
SoR3 <jystlik@gmail.com>
2021-11-30 01:17:49 +0700

    Добавьте файлы проекта.

---
+++ Tests/CalculatorTests.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using TestStack.White;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
+
+namespace AutoTestCalculate
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void Multiplicate()
+        {
+            var _ = Application.Launch(@"C:\Windows\system32\calc.exe");
+            Thread.Sleep(1000);
+            Process process = Process.GetProcesses().FirstOrDefault(x => x.MainWindowTitle.StartsWith("Calculator"));
+
+            Assert.IsNotNull(process);
+
+            Application application = Application.Attach(process.Id);
+            var window = application.GetWindow("Calculator");
+            window.Get<Button>("One").RaiseClickEvent();
+            window.Get<Button>("One").RaiseClickEvent();
+            window.Get<Button>("One").RaiseClickEvent();
+            window.Get<Button>("One").RaiseClickEvent();
+            window.Get<Button>("Multiply by").RaiseClickEvent();
+            window.Get<Button>("One").RaiseClickEvent();
+            window.Get<Button>("One").RaiseClickEvent();
+            window.Get<Button>("One").RaiseClickEvent();
+            window.Get<Button>("One").RaiseClickEvent();
+            window.Get<Button>("Equals").RaiseClickEvent();
+
+            var answer = window.GetElement(SearchCriteria.ByAutomationId("CalculatorResults")).Current.Name;
+
+            application.Close();
+
+            Assert.AreEqual(answer, "Display is 1 234 321");
+        }
+
+        [TestMethod]
+        public void Minus()
+        {
+            var _ = Application.Launch(@"C:\Windows\system32\calc.exe");
+            Thread.Sleep(1000);
+            Process process = Process.GetProcesses().FirstOrDefault(x => x.MainWindowTitle.StartsWith("Calculator"));
+
+            Assert.IsNotNull(process);
+
+            Application application = Application.Attach(process.Id);
+            var window = application.GetWindow("Calculator");
+
+            window.Get<Button>("Two").RaiseClickEvent();
+            window.Get<Button>("Two").RaiseClickEvent();
+            window.Get<Button>("Two").RaiseClickEvent();
+            window.Get<Button>("Two").RaiseClickEvent();
+            window.Get<Button>("Minus").RaiseClickEvent();
+            window.Get<Button>("One").RaiseClickEvent();
+            window.Get<Button>("One").RaiseClickEvent();
+            window.Get<Button>("One").RaiseClickEvent();
+            window.Get<Button>("One").RaiseClickEvent();
+            window.Get<Button>("Equals").RaiseClickEvent();
+
+            var answer = window.GetElement(SearchCriteria.ByAutomationId("CalculatorResults")).Current.Name;
+
+            application.Close();
+
+            Assert.AreEqual(answer, "Display is 5");
+        }
+    }
+}
---
+++ Tests/NotepadeTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using TestStack.White;
+using TestStack.White.Factory;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.WindowStripControls;
+
+namespace AutoTestNotepad
+{
+    [TestClass]
+    public class NotepadeTests
+    {
+        [TestMethod]
+        public void SavingFileTest()
+        {
+            var application = Application.Launch("notepad");
+
+            Assert.IsNotNull(application);
+
+            var mainWindow = application.GetWindow(SearchCriteria.ByText("Безымянный – Блокнот"), InitializeOption.NoCache);
+
+            mainWindow.Get<TextBox>(SearchCriteria.ByText("Текстовый редактор")).Enter("..,---->0_-");
+
+            var menu = mainWindow.Get<MenuBar>(SearchCriteria.ByAutomationId("MenuBar"));
+
+            menu.MenuItem("Файл", "Сохранить").Click();
+
+            var windowSaveFileDialog = mainWindow.ModalWindow("Сохранение");
+
+            windowSaveFileDialog.MdiChild(SearchCriteria.ByAutomationId("1001")).Enter(@"D:\test.txt");
+
+            windowSaveFileDialog.Get<Button>(SearchCriteria.ByText("Сохранить")).Click();
+
+            if (File.Exists(@"D:\test.txt"))
+            {
+                var windowConfirmationDialog = windowSaveFileDialog.ModalWindow("Подтвердить сохранение в виде");
+
+                if (windowConfirmationDialog != null)
+                    windowConfirmationDialog.Get<Button>(SearchCriteria.ByText("Да")).Click();
+            }
+
+            application.Close();
+
+            Assert.IsTrue(File.Exists(@"D:\test.txt"));
+            Assert.AreEqual(File.ReadAllText(@"D:\test.txt"), "..,---->0_-");
+        }
+    }
+}
